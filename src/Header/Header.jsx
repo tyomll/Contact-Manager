2a68fb60c05d98f03c6ce6403c846907fa8674c0
tyommmm <artyomhovsepyan135@gmail.com>
2022-10-25 21:05:00 +0400

    project setup

---
+++ src/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css"
+
+const Header = () => {
+    return(
+        <div className="header-container">
+            <h1>Contact Manager</h1>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/const.js
@@ -0,0 +1,13 @@
+import uuid from "react-uuid";
+
+const list = [
+  {
+    id: uuid(),
+    firstName: "",
+    lastName: "",
+    email : "",
+    phone : "",
+    profession: "",
+    avatar : "",
+  },
+];
